9614dbebcdbd3db9df1b82dd640cf61e0f9d97e4
tushargarg18 <tushargarg18@gmail.com>
2025-03-31 00:18:58 +0200

    Added time tracking for manual solver

--- manual_solver.py
+++ manual_solver.py
@@ -1,13 +1,14 @@
 import json
 import random
 from game_logic import Game_Logic
+import time
 
 class Manual_Solver(Game_Logic):
 
     def play(self):
         print("Minesweeper Started, Please enter coordinates as X,Y to proceed!")
         while True:
-            
+            start_time = time.time()
             Game_Logic.display(self)
             try:
                 row, col = map(int, input("Enter Row and Column:").split())
@@ -19,6 +20,8 @@ class Manual_Solver(Game_Logic):
             if self.grid[row][col] == "x" and not mode:
                 print("Boom you hit a mine!! GAME OVER :)")
                 print(self.grid)
+                end_time = time.time()
+                print(f"Manual solver completed in {end_time - start_time:.5f} seconds")
                 break
 
             Game_Logic.cell_Status(self, mode, row, col)
@@ -28,4 +31,6 @@ class Manual_Solver(Game_Logic):
             if all(self.revealed[x][y] or self.flagged[x][y] for x in range(self.size) for y in range(self.size)) or \
                sum(self.flagged[x][y] for x in range(self.size) for y in range(self.size)) == self.mines_count:
                 print("You Won !!!")
+                end_time = time.time()
+                print(f"Manual solver completed in {end_time - start_time:.5f} seconds")
                 break
